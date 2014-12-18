970553beb1ff74063e79a4b0b2a45cfc7a29aae4
RPE <rpe@tid.es>
2014-12-18 10:18:02 +0000

    UPDATE small advances deleting a map

--- awaza/datamodel.py
+++ awaza/datamodel.py
@@ -4,6 +4,8 @@ from random import randrange
 import sys
 
 client = RiakClient()
+MAXDATE = 99999999
+MINDATE = 00000000
 
 def extract_hour(time):
     """
@@ -23,44 +25,38 @@ def extract_dom(host):
 class User:
     def __init__(self, client, user_id):
         self.client = client
+        #self.maxdate = 99999999
+        #self.mindate = 00000000
         self.user_id = str(user_id)
         bucket_str = 'trackers'
         bucket = self.client.bucket_type('maps').bucket(bucket_str)
-
-        print self.user_id
-        # The Python client can use the new() function to automatically
-        # detect that a map is being targeted by the client
         key = self.user_id
         existing_map = bucket.get(key)
         self.tld_map = existing_map
 
-        """if existing_map.exists:
-            print "User exists\n"
-            self.tld_map = existing_map
-        else:
-            print "User do not exist. Creating\n"
-            self.tld_map = bucket.new(key)
-            #self.tld_map = Map(bucket, key)
-        """
     def visit_host(self, host, time):
         """
             Insert a visit to the host at the user's map
         """
-        print time
         hour = str(extract_hour(time))
-        print hour
         dom = extract_dom(host)
-        print dom
-    	#self.tld_map.maps[hour].counters[host].increment()
+        #if distance(maxdate)
+        #if time < self.maxdate:
         self.tld_map.maps[time].counters[dom].increment()
     	self.tld_map.store()
+
+    def remove_visit(self, host, time):
+        """
+            Remove a visit to the host at the user's map
+        """
+        self.tld_map.maps[time].clear()
+
+
 
 def get_random_host(max_doms):
     """
         Given a number of doms, XX, return domXX.com
     """
-    #hosts = ["elpais.com", "google.com", "raquel.com"]
-    #random_host = hosts[randrange(len(hosts)     )]
     random_host = "dom_" + str(randrange(max_doms)).zfill(3) + ".com"
     return random_host
 
@@ -69,7 +65,7 @@ def create_all_user_entries(user_id, max_doms, max_user_visits):
         Given max_user_visits and a user, do random visits to the listed hosts till arrive to max_user_visits
     """
     user = User(client, user_id)
-    for visit_hour in range(0,1):
+    for visit_hour in range(0,23):
         time = "20141214" + str(visit_hour).zfill(2)
         for visit_number in range(0, max_user_visits):
             host = get_random_host(max_doms)
@@ -81,11 +77,10 @@ def create_all_entries():
         Defining maximum values for users ids and
         visits by user in an hour
     """
-    max_user_id = 2
-    max_doms = 1000;
+    max_user_id = 10
+    max_doms = 1000
     max_user_visits = 5
-    #for user_id in range(0, max_user_id)
-    for user_id in range(0, 1):
+    for user_id in range(0, max_user_id):
       create_all_user_entries(0, max_doms, max_user_visits)
 
 
@@ -102,16 +97,22 @@ def query_stats_by_user(client, user_id):
             else :
                 current24[tld[0]] = 1
                 print(current24)
-    yield current24
+    return current24
     
 
 def query_stats():
     current_user_id = 0;
     user_dict = query_stats_by_user(client, current_user_id)
 
-
-
-
+def cleaner(client, users):
+    for i in range(users):
+        pass
+def reader(client, n, users, tlds, times):
+    #todo date
+    for i in range(n):
+        pass
+def writer():
+    pass
 if __name__ == '__main__':
 	#usuario = User(client, '001')
     #create_all_entries()
